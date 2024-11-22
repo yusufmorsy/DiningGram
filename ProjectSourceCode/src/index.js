89ae264de8ee7fb82d4cb1dd17c91aa870f9a0cc
Rodolfo Martinez-Maldonado <mandm.rodolfo@gmail.com>
2024-11-22 10:41:34 -0700

    IMPORTANT: Modified postgres host in index.js AND env

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -25,7 +25,7 @@ const hbs = handlebars.create({
 
 // database configuration
 const dbConfig = {
-  host: 'db', // the database server
+  host: process.env.POSTGRES_HOST, // the database server
   port: 5432, // the database port
   database: process.env.POSTGRES_DB, // the database name
   user: process.env.POSTGRES_USER, // the user account to connect with
